ec7e2be52be9de5341586ad80c840a6e986ce478
jiyoon <skymap02@naver.com>
2023-11-23 18:58:19 +0900

    23.11.23 강의

---
+++ Javascript_a-z/project1.StopWatch/script.js
@@ -0,0 +1,38 @@
+const appendTens = document.getElementById('tens');
+const appendSeconds = document.getElementById('seconds');
+const buttonStart = document.getElementById('button-start');
+const buttonStop = document.getElementById('button-stop');
+const buttonReset = document.getElementById('button-reset');
+
+let seconds = 0;
+let tens = 0;
+let interval;
+
+buttonStart.onclick = function () {
+    interval = setInterval(startTimer, 10);
+}
+
+buttonStop.onclick = function () {
+    clearInterval(interval);
+}
+
+buttonReset.onclick = function () {
+    clearInterval(interval);
+    seconds = 0;
+    tens = 0;
+    appendSeconds.innerText = seconds;
+    appendTens.innerText = tens;
+}
+function startTimer() {
+    tens++;
+
+    if(tens > 99){
+        seconds++;
+        appendSeconds.innerText = seconds;
+        tens = 0;
+        appendTens.innerText = tens;
+    } else{
+        appendTens.innerText = tens;
+    }
+}
+
